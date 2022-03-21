f03fe2c01f11858ddd65b7848c26486113b31cf0
Carlos Martinez <ing.cemh.871208@gmail.com>
2022-03-21 13:34:38 -0400

    STM32F401_baseline: main.c configured hello world blinking LED

--- src/main.c
+++ src/main.c
@@ -25,23 +25,36 @@
 /*                                                       Includes                                                    */
 /*********************************************************************************************************************/
 #include "std_types.h"
+#include "stm32F401re.h"
 
 /*                                                Constants and types                                                */
 /*********************************************************************************************************************/
-uint8 unsignedVariable      = 0;
-static uint8 staticVariable = 0;
-const uint8 constVariable   = 0; 
+#define LED_GPIO        GPIOA
+#define LED_PIN         5
+
+static void delay(unsigned time)
+{
+    for (unsigned i=0; i<time; i++)
+        for (volatile unsigned j=0; j<20000; j++);
+}
 
 /*                                           Main function implementation                                            */
 /*********************************************************************************************************************/
 int main (void)
 {
-   unsignedVariable = 1;
-   staticVariable   = 1;
-   for(;;){}
-   return EXIT_PROGRAM;
+    RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOAEN; /*Enable clock to GPIOA*/
+    LED_GPIO->MODER |= (0b01 << (LED_PIN << 1)); /*Set LED pin as output*/   
+    for(;;)
+    {
+        LED_GPIO->BSRR = (1 << LED_PIN); /*Set LED pin ON*/
+        delay(200);
+        LED_GPIO->BSRR = (1 << (LED_PIN + 16)); /*Set LED pin OFF*/
+        delay(200);
+    }
+    return EXIT_PROGRAM;
 }
 /***************************************************Log Projects*******************************************************
  *|    ID   | JIRA Ticket |     Date    |                                Description                                  |
  *| CMARTI  |     NA      | 17-Mar-2022 | main.c standard version.                                                    |
+ *| CMARTI  |     NA      | 21-Mar-2022 | main.c Hello world program: blinking LED                                    |
 **********************************************************************************************************************/
