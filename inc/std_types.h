72b46f68c20e52798c82e3737a87c90dee4b12ae
Carlos Martinez <ing.cemh.871208@gmail.com>
2022-08-22 12:27:04 -0400

    SAM-3: Added PORTDriver types to Port.h

--- inc/std_types.h
+++ inc/std_types.h
@@ -4,7 +4,7 @@
 /*                                               OBJECT SPECIFICATION                                                */
 /*********************************************************************************************************************/
 /*!
- * $Source: main.c $
+ * $Source: std_types.h $
  * $Revision: Version 1.0 $
  * $Author: Carlos Martinez $
  * $Date: 2022-03-02 $
@@ -53,6 +53,45 @@ typedef uint8 boolean;
 
 /*Exit Program*/
 #define EXIT_PROGRAM (boolean) 0u
+
+/*Symbol definitions*/
+#ifndef STATUSTYPEDEFINED
+#define STATUSTYPEDEFINED
+typedef unsigned char StatusType; /*OSEK compliance*/
+#define E_OK       0x00u
+#endif
+#define E_NOT_OK   0x01u
+
+#define STD_HIGH   0x01u /* Physical state 5V or 3.3V */ 
+#define STD_LOW    0x00u /* Physical state 0V */
+
+#define STD_ACTIVE 0x01u /* #define STD_IDLE 0x00u */
+#define STD_IDLE   0x00u /* Logical state idle */
+
+#define STD_OFF    0x00u
+#define STD_ON     0x01u
+
+/*
+* Std_ReturnType
+* This type can be used as standard API return type which is shared between the RTE and the BSW modules. It shall be 
+* defined as follows: typedef uint8 Std_ReturnType;
+*/
+typedef uint8 Std_ReturnType;
+
+/*
+* Std_VersionInfoType
+* This type shall be used to request the version of a BSW module using the <Module name>_GetVersionInfo() function.
+*/
+typedef struct
+{
+    /* data */
+    uint16 vendorID;
+    uint16 moduleID;
+    uint8  sw_major_version;
+    uint8  sw_minor_version;
+    uint8  sw_patch_version;
+}Std_VersionInfoType;
+
 /*                                                    Exported Variables                                             */
 /*********************************************************************************************************************/
 
--- src/BSW/MCAL/IODrivers/PORTDrivers/Port.h
+++ src/BSW/MCAL/IODrivers/PORTDrivers/Port.h
@@ -7,7 +7,7 @@
  * $Source: Port.h $
  * $Revision: Version 1.0 $
  * $Author: Carlos Martinez $
- * $Date: 2022-08-18 $
+ * $Date: 2022-08-19 $
  */
 /*********************************************************************************************************************/
 /* DESCRIPTION :                                                                                                     */
@@ -25,18 +25,89 @@
 #define PORT_H_
 /*                                                       Includes                                                    */
 /*********************************************************************************************************************/
-
+#include "std_types.h"
+#include "stm32F401re.h"
 /*                                                Constants and types                                                */
 /*********************************************************************************************************************/
+/*
+* Port_ConfigType
+* Type of the external data structure containing the initialization data for this module.
+* The contents of the initialization data structure are specific to the microcontroller.
+*/
+#define PORTA 0u
+#define PORTB 1u
+#define PORTC 2u
+#define PORTD 3u
+#define PORTE 4u
+#define PORTH 5u
+
+const uint32_t RCCAHB1ENRGPIOMask[6] = {RCC_AHB1ENR_GPIOAEN,RCC_AHB1ENR_GPIOBEN,RCC_AHB1ENR_GPIOCEN,RCC_AHB1ENR_GPIODEN
+                                        RCC_AHB1ENR_GPIOEEN,RCC_AHB1ENR_GPIOHEN};
+typedef struct 
+{
+    RCC_TypeDef*  GPIOPortClkEn;
+    GPIO_TypeDef* GPIOPortPinCfg[6] = {GPIOA,GPIOB,GPIOC,GPIOD,GPIOE,GPIOH};
+}Port_ConfigType;
+
+/*
+* Port_PinType
+* Data type for the symbolic name of a port pin.
+*/
+typedef uint16 Port_PinType;
+
+/*
+* Port_PinDirectionType
+* Possible directions of a port pin.
+*/
+typedef uint8 Port_PinDirectionType;
+#define PORT_PIN_IN  ((Port_PinDirectionType)0x00u) /*Sets port pin as input.*/
+#define PORT_PIN_OUT ((Port_PinDirectionType)0x01u) /*Sets port pin as output.*/
+
+/*
+* Port_PinModeType
+* Different port pin modes.
+* As several port pin modes shall be configurable on one pin, the range shall be determined by the implementation.
+*/
+typedef uint32 Port_PinModeType;
 
 /*                                                    Exported Variables                                             */
 /*********************************************************************************************************************/
 
 /*                                              Exported functions prototypes                                        */
 /*********************************************************************************************************************/
+/*
+* Port_Init
+* Initializes the Port Driver module.
+*/
+void Port_Init (const Port_ConfigType* ConfigPtr);
+
+/*
+* Port_SetPinDirection
+* Sets the port pin direction
+*/
+void Port_SetPinDirection (Port_PinType Pin, Port_PinDirectionType Direction);
+
+/*
+* Port_RefreshPortDirection
+* Refreshes port direction.
+*/
+void Port_RefreshPortDirection (void);
+
+/*
+* Port_GetVersionInfo
+* Returns the version information of this module.
+*/
+void Port_GetVersionInfo (Std_VersionInfoType* versioninfo);
+
+/*
+* Port_SetPinMode
+* Sets the port pin mode.
+*/
+void Port_SetPinMode (Port_PinType Pin, Port_PinModeType Mode);
 
 #endif
 /***************************************************Log Projects*******************************************************
  *|    ID   | JIRA Ticket |     Date    |                                Description                                  |
  *| CMARTI  |    SAM-2    | 18-Aug-2022 | First version of Port.h                                                     |
+ *| CMARTI  |    SAM-3    | 22-Aug-2022 | Added PORTDriver autosar types                                              |
 **********************************************************************************************************************/
